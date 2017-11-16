7058ac31c7a54927597071f42258f3f12423f6eb
Christopher Celio <celio@eecs.berkeley.edu>
2017-11-16 14:27:57 -0800

    Add riscv-torture submodule.

--- scripts/init-submodules.sh
+++ scripts/init-submodules.sh
@@ -8,4 +8,4 @@ git submodule update --init
 (cd rocket-chip && git submodule update --init riscv-tools)
 (cd rocket-chip/riscv-tools && git submodule update --init --recursive riscv-isa-sim riscv-fesvr riscv-pk riscv-opcodes riscv-tests riscv-gnu-toolchain riscv-openocd)
 # torture submodules
-#(cd torture && git submodule update --init --recursive)
+(cd torture && git submodule update --init --recursive)
