6b63c74feec0b1eb3162f41e75f598b9ffc77cf3
Jiyong Yu <jiyongy2@weapon.cs.illinois.edu>
2018-07-14 14:08:10 -0500

    implement cmov on boom

--- benchmarks-old/L2_dataset/cisc_oblivious/condor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-os.system("make clean")
-os.system("make sim")
-os.system("rm ./result/*")
-os.system("rm ./condor_out/*")
-os.system("condor_submit condor_jobs")
