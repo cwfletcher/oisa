2376f445f9105783fd26d5d65e172ebad11a828c
Jiyong Yu <jiyongy2@weapon.cs.illinois.edu>
2018-07-16 11:51:51 -0500

    before applyinf riscv-tests as solution to bypass syscalls

---
+++ benchmarks/cmov.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = a+b;
+    printf("Hello world\n");
+}
--- benchmarks/include/asm.h
+++ benchmarks/include/asm.h
@@ -19,7 +19,7 @@
                             "sw t1,(%2)"                            \
                             :                                       \
                             : "r"(if_mov), "r"(src), "r"(dst_addr)  \
-                            : "t1");                                \
+                            : "t1", "memory");                      \
 
 #define _cmov_src_addr(if_mov, src_addr, dst_addr)                      \
     __asm__ __volatile__ (  "lw t1,(%1)\n\t"                            \
@@ -28,7 +28,7 @@
                             "sw t2,(%2)"                                \
                             :                                           \
                             : "r"(if_mov), "r"(src_addr), "r"(dst_addr) \
-                            : "t1", "t2");                              \
+                            : "t1", "t2", "memory");                    \
 
 #define _oswap(if_swap, op1_addr, op2_addr)                                 \
     __asm__ __volatile__ (  "lw t1,(%1)\n\t"                                \
@@ -40,7 +40,7 @@
                             "sw t2,(%2)"                                    \
                             :                                               \
                             : "r"(if_swap), "r"(op1_addr), "r"(op2_addr)    \
-                            : "t1", "t2", "t3");                            \
+                            : "t1", "t2", "t3", "memory");                  \
 
 inline void _cmovn(int if_mov, void* src_addr, void* dst_addr, int len) {
     void* curr_src_addr = src_addr;
@@ -62,4 +62,42 @@ inline void _oswapn(int if_swap, void* op1_addr, void* op2_addr, int len){
     }
 }
 
+/* Backup functions
+inline void _rdtsc(counter){
+    __asm__ __volatile__ (  "csrrs %0,cycle,x0"
+                            : "=r"(counter) );
+}
+
+inline void _cmov(if_mov, src, dst_addr){
+    __asm__ __volatile__ (  "lw t1,(%2)\n\t"
+                            "cmovw t1,%0,%1\n\t"
+                            "sw t1,(%2)"
+                            :
+                            : "r"(if_mov), "r"(src), "r"(dst_addr)
+                            : "t1");
+}
+
+inline void _cmov_src_addr(int if_mov, void* src_addr, void* dst_addr){
+    __asm__ __volatile__ (  "lw t1,(%1)\n\t"
+                            "lw t2,(%2)\n\t"
+                            "cmovw t2,%0,t1\n\t"
+                            "sw t2,(%2)"
+                            :
+                            : "r"(if_mov), "r"(src_addr), "r"(dst_addr)
+                            : "t1", "t2");
+}
+
+inline void _oswap(int if_swap, void* op1_addr, void* op2_addr){                                 \
+    __asm__ __volatile__ (  "lw t1,(%1)\n\t"
+                            "lw t2,(%2)\n\t"
+                            "mv t3,t2\n\t"
+                            "cmovw t2,%0,t1\n\t"
+                            "cmovw t1,%0,t3\n\t"
+                            "sw t1,(%1)\n\t"
+                            "sw t2,(%2)"
+                            :
+                            : "r"(if_swap), "r"(op1_addr), "r"(op2_addr)
+                            : "t1", "t2", "t3");
+}
+*/
 #endif
