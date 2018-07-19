a2c606fa8a299b2f1d3b86a37c1101933acad45e
Jiyong Yu <jiyongy2@weapon.cs.illinois.edu>
2018-07-19 16:57:03 -0500

    add ift logic for risc, non-mem instructions. including branch check and rollback

--- benchmarks/cmov.c
+++ benchmarks/cmov.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "include/asm.h"
+
 int main() {
     int a = 1;
     int b = 2;
-    int c = a+b;
-    printf("Hello world\n");
+    int c = 3;
+    int t1 = 0;
+    int t2 = 0;
+
+    _rdtsc(t1);
+    c = a + b;
+    _rdtsc(t2);
+
+    printf("a=%d, b=%d, c=%d, t1=%d, t2=%d\n", a,b,c,t1,t2);
 }
